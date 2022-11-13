f15590fee6f0e9a10b96c56ee7a731f6f1d8810b
Caramel <58130816+Wen0327@users.noreply.github.com>
2022-11-13 11:57:23 +0800

    Add files via upload

---
+++ Wen/js/sidebar.js
@@ -0,0 +1,29 @@
+
+
+function openSlideMenu(){
+    document.getElementById('side-menu').style.width='250px';
+    //document.getElementById('content').style.marginLeft='250px';
+    //document.getElementById('footer').style.marginLeft='250px';
+}
+
+
+function closeSlideMenu(){
+    document.getElementById('side-menu').style.width='0';
+   // document.getElementById('content').style.marginLeft='0';
+    //document.getElementById('footer').style.marginLeft='0';
+}
+
+function goIG(){  
+    window.location.href='https://www.instagram.com/wenyu_0327/';
+}
+
+function goFB(){ 
+    window.location.href='https://www.facebook.com/profile.php?id=100003208639614';
+}
+
+function openTWT(){
+    window.location.href='https://twitter.com/Caramel_Wen';
+}
+
+
+
